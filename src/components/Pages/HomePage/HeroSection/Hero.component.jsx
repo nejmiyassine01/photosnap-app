b42b804445b21261b45e52c2a06440281d74ebde
nejmiyassine01 <nejmi.yassine01@gmail.com>
2020-12-12 20:14:32 +0100

    Update Responsive

--- src/components/Pages/HomePage/HeroSection/Hero.component.jsx
+++ src/components/Pages/HomePage/HeroSection/Hero.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import { Button } from '../Button/Button.component';
 import heroImage from '../../../../assets/home/desktop/create-and-share.jpg'
-
+// import heroImageTablet from '../../../../assets/home/tablet/create-and-share.jpg'
+// import heroImageMobile from '../../../../assets/home/mobile/create-and-share.jpg'
 import './Hero.styles.scss';
 
 function Hero() {
@@ -11,7 +11,7 @@ function Hero() {
         <section className="heroSection">
             <div className="heroSection__container">
                 <div className="heroSection__container--content">
-                    <h1>CREATE AND SHARE YOUR PHOTO STORIES.</h1>
+                    <h2>CREATE AND SHARE YOUR PHOTO STORIES.</h2>
                     <p>Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.</p>    
 
                     <Router>
@@ -21,6 +21,15 @@ function Hero() {
             </div>
             
             <div className="heroSection__image">
+            {/* <picture>
+                    <source srcSet={`${heroImageTablet} 1x`} media="(max-width: 1190px)" />
+                    <source srcSet={`${heroImageTablet} 1x`} media="(max-width: 901px)" />
+                    <img
+                        className=""
+                        srcSet={`${heroImage} 2x`}
+                        alt="testimonialsImg" 
+                    />
+                </picture> */}
                 <img src={heroImage} alt="hero" />
             </div>
         </section>
